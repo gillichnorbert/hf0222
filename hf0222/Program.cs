75f9613b6d2d5456c1ca364ff7ded7a336a0ae98
Gillich Norbert <gillich.norbert@icloud.com>
2024-02-22 18:34:53 +0100

    Elso

---
+++ hf0222/Program.cs
@@ -0,0 +1,9 @@
+﻿using hf0222;
+
+string szo1 = "ElsőSzó";
+string szo2 = "MásodikSzó";
+SzovegOsszefuzo osszefuzo = new SzovegOsszefuzo(szo1, szo2);
+string eredmeny = osszefuzo.SzoOsszefuzo();
+Console.WriteLine(eredmeny);
+
+Console.ReadKey();
---
+++ hf0222/Szavak.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace hf0222
+{
+	public class SzovegOsszefuzo
+    {
+		private string szo1;
+		private string szo2;
+
+        public SzovegOsszefuzo(string szo1, string szo2)
+        {
+            this.szo1 = szo1;
+            this.szo2 = szo2;
+        }
+
+        public string SzoOsszefuzo()
+        {
+            string osszefuzott = "";
+
+            string rovidebb = szo1.Length <= szo2.Length ? szo1 : szo2;
+            string hosszabb = szo1.Length > szo2.Length ? szo1 : szo2;
+            
+
+            for (int i = 0; i < rovidebb.Length; i++)
+            {
+                osszefuzott += szo1[i];
+                osszefuzott += szo2[i];
+            }
+
+
+
+            return osszefuzott + hosszabb.Substring(rovidebb.Length);
+        }
+    }
+}
+
